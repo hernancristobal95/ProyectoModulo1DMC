c58f2884d0db8eda38f2ed9327a52cd1bd94052b
Hernan Cristobal <hernancristobal23@gmail.com>
2026-07-07 22:26:44 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,4 +2,4 @@ import streamlit as st
 st.title("Proyecto módulo 1 Fundamentals")
 st.sidebar.title("Parámetros")
 
-valor_inicial = ist.number_input("Ingrese el valor inicial")
+valor_inicial = st.number_input("Ingrese el valor inicial")
