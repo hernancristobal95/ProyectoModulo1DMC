bec8f35ad1fd20265a01817d5d77b9b5725907d0
Hernan Cristobal <hernancristobal23@gmail.com>
2026-07-25 10:51:07 -0500

    Update app.py

--- app.py
+++ app.py
@@ -22,13 +22,13 @@ if modulo == "Home":
   
 elif modulo == "Ejercicio 1":
   st.title("Ejercicio 1")
-  st.markdown("### Descripción
+  st.markdown("""### Descripción
   Registre los movimientos financieros de una empresa.
   Cada movimiento debe incluir:
   - Concepto
   - Tipo de movimiento (Ingreso o Gasto)
   - Valor
-  Al final se mostrará el total de ingresos, gastos, saldo y el estado del flujo de caja.")
+  Al final se mostrará el total de ingresos, gastos, saldo y el estado del flujo de caja.""")
   st.write("Estas en el módulo de arreglos")
   valor_inicial = st.number_input("Ingrese el valor inicial", value = 0)
   valor_final = st.number_input("Ingrese el valor final", value = 1)
